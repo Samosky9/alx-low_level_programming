6a4da1bb09588cbb2e2016882d3cff1e2a2036d3
Samosky9 <haryanwolesamuel27@gmail.com>
2023-08-15 20:18:55 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	long int a, b, c, d;
+
+	b = 1;
+	c = 2;
+	d = 3;
+
+	for (a = 0; a < 50; a++)
+	{
+		if (a != 48)
+			printf("%ld, ", b);
+		else
+			printf("%ld\n", b);
+		d = c + d;
+		c = b + c;
+		b = d - c;
+	}
+	return (0);
+}
