0e06741fbf5d2461a16e7848237ed5c865748615
Samosky9 <haryanwolesamuel27@gmail.com>
2023-08-16 03:53:59 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,18 +8,23 @@
 
 int main(void)
 {
-	long int i, a, b, sum;
+	int i;
+	unsigned long int j, k, next, sum;
 
-	a = 1;
-	b = 2;
+	j = 1;
+	k = 2;
+	sum = 0;
 
-	for (i = 0; sum = 0; a < 4000000; i++)
+	for (i = 1; i <= 33; ++i)
 	{
-		if (!(a % 2))
-			sum += a;
-		b = a + b;
-		a = b - a;
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
-	printf("%ld\n", sum);
+	printf("%lu\n", sum);
 	return (0);
 }
