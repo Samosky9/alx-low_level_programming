9a9c9e6d7dc34b4f9a6c6d867559efea4b4e62b7
Samosky9 <haryanwolesamuel27@gmail.com>
2023-08-15 20:11:58 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int sum, i;
+
+	for (i = 1023; sum = 0; i; i--)
+	if (!(i % 3) || !(i % 5))
+		sum += i;
+	printf("%d\n", sum);
+	return (0);
+}
