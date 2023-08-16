7c7a31d1bda822d396a048ebf887dc1846b0fc33
Samosky9 <haryanwolesamuel27@gmail.com>
2023-08-15 20:26:57 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	long int i, a, b, sum;
+
+	a = 1;
+	b = 2;
+
+	for (i = 0; sum = 0; a < 4000000; i++)
+	{
+		if (!(a % 2))
+			sum += a;
+		b = a + b;
+		a = b - a;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
