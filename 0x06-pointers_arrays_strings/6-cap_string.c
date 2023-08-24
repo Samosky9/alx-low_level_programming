b8e219bfd63be85330234d12473fb075bd33c2f9
Samosky9 <haryanwolesamuel27@gmail.com>
2023-08-24 01:13:31 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,9 +11,9 @@ char *cap_string(char *a)
 	int i, x;
 	char *separators = ",;.!?\"(){} \n\t";
 
-	for (i = 0; *(a + 1) != '\0'; i++)
+	for (i = 0; *(a + i) != '\0'; i++)
 	{
-		if (*(a + 1) >= 'a' && *(a + 1) <= 'z')
+		if (*(a + i) >= 'a' && *(a + i) <= 'z')
 			for (x = 0; *(separators + x) != '\0'; x++)
 			{
 				if (*(a + i - 1) == *(separators + x))
