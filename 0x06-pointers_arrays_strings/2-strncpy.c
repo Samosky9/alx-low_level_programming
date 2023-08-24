574b517d9bb95a588983bf4e0bff32f6666821d2
Samosky9 <haryanwolesamuel27@gmail.com>
2023-08-23 20:40:40 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy two strings only taking n bytes from src
+ * @dest: first string to be added
+ * @src: second string to be added
+ * @n: number of bytes to use from src
+ * Return: concatenated string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *temp = dest;
+
+	for (; n != 0 && *src != '\0'; n--, src++)
+	{
+		*temp = *src;
+		temp++;
+	}
+	for (; n != 0 && *temp != '\0'; n--, temp++)
+	{
+		temp = '\0';
+	}
+	return (dest);
+}
