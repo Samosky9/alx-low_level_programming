eda4424f92669510afab4d654fe86c1e0374f800
Samosky9 <haryanwolesamuel27@gmail.com>
2023-08-25 06:26:57 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -38,15 +38,16 @@ void print_buffer(char *b, int size)
 
 	for (i = 0; i < j; i++)
 	{
-		int c = *(b + o + i);
+	int c = *(b + o + i);
 
-		if (c < 32 || c > 132)
-		{
-			c = '.';
-		}
-		printf("%c", c);
+	if (c < 32 || c > 132)
+	{
+		c = '.';
+	}
+	printf("%c", c);
 	}
 	printf("\n");
 	o += 10;
+	}
 	}
 }
