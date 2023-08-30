fda1e04cc28f04adf43631401ac8b5a2ab14be0f
Samosky9 <haryanwolesamuel27@gmail.com>
2023-08-30 12:42:27 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,36 +9,23 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0')
 	{
-		return (1);
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
 	}
 
-	if (*s1 ==  *s2 || *s2 == '*')
+	if (*s2 == '\0')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+
+	else if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
-
-	if (*s2 == '*')
-	{
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-	}
-	return (0);
-}
-
-int main(void)
-{
-	char str1[] = "hello";
-	char str2[] = "h*lo";
-
-	if (wildcmp(str1, str2))
-	{
-		printf("The strings are identical.\n");
-	}
-
-	else
-	{
-		printf("The strings are not identical.\n");
 	}
 	return (0);
 }
