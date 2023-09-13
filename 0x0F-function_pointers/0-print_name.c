338d056789c9ef40fe991a5c0144f312c67de9b2
Samosky9 <haryanwolesamuel27@gmail.com>
2023-09-13 06:42:29 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,4 +2,12 @@
 
 /**
  * print_name - function that prints a name
- *
+ * @name: name to be printed
+ * @f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		f(name);
+}
